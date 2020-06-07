18ad8e79c9f966313b4f5b354c71c753bf16885b
Jitgitbit <rowowolf@gmail.com>
2020-06-07 14:04:39 +0200

    [-1], we are accessing the last item in the list

--- blockchain.py
+++ blockchain.py
@@ -1,1 +1,14 @@
-print(2 ** 3)
+# def power_fn():
+#     print(2 ** 3)
+
+# power_fn()
+
+blockchain = [1]
+
+def add_value():
+    blockchain.append([blockchain[-1], 5.3])              
+    print(blockchain)
+
+add_value()
+add_value()
+add_value()
--- comments.md
+++ comments.md
@@ -1,1 +1,3 @@
 python blockchain.py
+
+blockchain.append([blockchain[-1], 5.3])   (--> with [-1], we are accessing the last item in the list)
