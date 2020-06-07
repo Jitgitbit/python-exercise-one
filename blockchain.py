3d8da77fc70b33e6d76dfc9f8531dab241bd8b04
Jitgitbit <rowowolf@gmail.com>
2020-06-07 16:11:22 +0200

    Created a "for" Loop for our blockchain

--- blockchain.py
+++ blockchain.py
@@ -30,4 +30,9 @@ add_value(last_transaction=get_last_blockchain_value(), transaction_amount=tx_am
 tx_amount = get_user_input()
 add_value(tx_amount, get_last_blockchain_value())
 
-print(blockchain)
+# Output the blockchain list to the console
+for block in blockchain:
+    print('Ouputting block')
+    print(block)
+
+print('Done!')
