f6a61f3d61db15f3a26cef2a7935004642ab7ff3
Jitgitbit <rowowolf@gmail.com>
2020-06-07 16:14:24 +0200

    improved readability

--- blockchain.py
+++ blockchain.py
@@ -21,12 +21,15 @@ def get_user_input():
     """ Returns the input of the user (a new transaction) as a float. """
     return float(input('Input your transaction amount please: '))       
 
+# Get the first transaction input and add the value to the blockchain
 tx_amount = get_user_input()
 add_value(tx_amount)
 
+# Get the second transaction input and add the value to the blockchain
 tx_amount = get_user_input()
 add_value(last_transaction=get_last_blockchain_value(), transaction_amount=tx_amount)
 
+# Get the third transaction input and add the value to the blockchain
 tx_amount = get_user_input()
 add_value(tx_amount, get_last_blockchain_value())
 
