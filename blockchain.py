333989125f0332b7e25d1ddc4ab63306657af50e
Jitgitbit <rowowolf@gmail.com>
2020-06-09 21:00:51 +0200

    bug fix: because Dictionaries are unordered, we have to add sort_keys=True

--- blockchain.py
+++ blockchain.py
@@ -28,7 +28,7 @@ def hash_block(block):
         Arguments:
             :block: The block that should be hashed.
     """
-    return hl.sha256(json.dumps(block).encode()).hexdigest()
+    return hl.sha256(json.dumps(block, sort_keys=True).encode()).hexdigest()
 
 
 def valid_proof(transactions, last_hash, proof):
