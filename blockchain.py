9e5cdbe6062a4bfe60794e0818fd1efd5ba29a15
Jitgitbit <rowowolf@gmail.com>
2020-06-09 20:31:59 +0200

    ! Proof of Work fn added !

--- blockchain.py
+++ blockchain.py
@@ -33,8 +33,17 @@ def hash_block(block):
 def valid_proof(transactions, last_hash, proof):
     guess = (str(transactions) + str(last_hash) + str(proof)).encode()
     guess_hash = hl.sha256(guess).hexdigest()
-    print(guess_hash)
+    print('*****-> What is guess_hash in valid_proof: ', guess_hash)
     return guess_hash[0:2] == '00'
+
+
+def proof_of_work():
+    last_block = blockchain[-1]
+    last_hash = hash_block(last_block)
+    proof = 0
+    while not valid_proof(open_transactions, last_hash, proof):
+        proof += 1
+    return proof
 
 
 def get_balance(participant):
