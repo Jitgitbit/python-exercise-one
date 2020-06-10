69fa7b3030fa8679b4b240124cf95f7ccef2e1b0
Jitgitbit <rowowolf@gmail.com>
2020-06-10 14:44:33 +0200

    using the Transaction class

--- transaction.py
+++ transaction.py
@@ -1,3 +1,4 @@
+from collections import OrderedDict
 
 
 class Transaction:
@@ -5,3 +6,7 @@ class Transaction:
         self.sender = sender
         self.recipient = recipient
         self.amount = amount
+
+
+    def to_ordered_dict(self):
+        return OrderedDict([('sender', self.sender), ('recipient', self.recipient), ('amount', self.amount)])
