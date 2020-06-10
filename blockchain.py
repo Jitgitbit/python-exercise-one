1e55640f61945d8a04ea881421beea26df8812aa
Jitgitbit <rowowolf@gmail.com>
2020-06-10 23:11:46 +0200

    Added a "Verification" Helper Class

--- blockchain.py
+++ blockchain.py
@@ -77,24 +77,6 @@ def save_data():
         print('Saving failed!')
 
 
-def valid_proof(transactions, last_hash, proof):
-    """Validate a proof of work number and see if it solves the puzzle algorithm (two leading 0s)
-
-		Arguments:
-			:transactions: The transactions of the block for which the proof is created.
-			:last_hash: The previous block's hash which will be stored in the current block.
-			:proof: The proof number we're testing.
-		"""
-    # Create a string with all the hash inputs
-    guess = (str([tx.to_ordered_dict() for tx in transactions]) + str(last_hash) + str(proof)).encode()
-    # Hash the string
-    # IMPORTANT: This is NOT the same hash as will be stored in the previous_hash. It's a not a block's hash. It's only used for the proof-of-work algorithm.
-    guess_hash = hash_string_256(guess)
-    # Only a hash (which is based on the above inputs) which starts with two 0s is treated as valid
-    # This condition is of course defined by you. You could also require 10 leading 0s - this would take significantly longer (and this allows you to control the speed at which new blocks can be added)
-    return guess_hash[0:2] == '00'
-
-
 def proof_of_work():
     """Generate a proof of work for the open transactions, the hash of the previous block and a random number (which is guessed until it fits)."""
     last_block = blockchain[-1]
@@ -134,20 +116,6 @@ def get_last_blockchain_value():
     if len(blockchain) < 1:
         return None
     return blockchain[-1]
-
-
-def verify_transaction(transaction):
-    """Verify a transaction by checking whether the sender has sufficient coins.
-
-		Arguments:
-			:transaction: The transaction that should be verified.
-		"""
-    sender_balance = get_balance(transaction.sender)
-    return sender_balance >= transaction.amount
-
-# This function accepts two arguments.
-# One required one (transaction_amount) and one optional one (last_transaction)
-# The optional one is optional because it has a default value => [1]
 
 
 def add_transaction(recipient, sender=owner, amount=1.0):
@@ -215,24 +183,6 @@ def print_blockchain_elements():
         print(' => Outputting Block: ', block)
     else:
         print('-' * 40)
-
-
-def verify_chain():
-    """ Verify the current blockchain and return True if it's valid, False otherwise."""
-    for (index, block) in enumerate(blockchain):
-        if index == 0:
-            continue
-        if block.previous_hash != hash_block(blockchain[index - 1]):
-            return False
-        if not valid_proof(block.transactions[:-1], block.previous_hash, block.proof):
-            print(' -> Proof of work is invalid')
-            return False
-    return True
-
-
-def verify_transactions():
-    """Verifies all open transactions."""
-    return all([verify_transaction(tx) for tx in open_transactions])
 
 
 waiting_for_input = True
