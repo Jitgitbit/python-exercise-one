430cfe47d4d266f2a2e9be490383bd51c58a9fed
Jitgitbit <rowowolf@gmail.com>
2020-06-11 21:39:03 +0200

    Verifying Signatures

--- blockchain.py
+++ blockchain.py
@@ -9,6 +9,7 @@ from utility.verification import Verification
 from utility.hash_util import hash_block
 from block import Block
 from transaction import Transaction
+from wallet import Wallet
 
 # The reward we give to miners (for creating a new block)
 MINING_REWARD = 10
@@ -158,6 +159,8 @@ class Blockchain:
         if self.hosting_node == None:
             return False
         transaction = Transaction(sender, recipient, signature, amount)
+        if not Wallet.verify_signature(transaction):
+            return False
         if Verification.verify_transaction(transaction, self.get_balance):
             self.__open_transactions.append(transaction)
             self.save_data()
@@ -186,6 +189,9 @@ class Blockchain:
         copied_transactions.append(reward_transaction)
         block = Block(len(self.__chain), hashed_block,
                       copied_transactions, proof)
+        for tx in block.transactions:
+            if not Wallet.verify_signature(tx):
+                return False
         self.__chain.append(block)
         self.__open_transactions = []
         self.save_data()
